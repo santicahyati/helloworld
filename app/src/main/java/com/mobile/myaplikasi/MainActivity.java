3bd2d9526afc53969e58d0bbab6b3dc1b95a4ed8
Santi Cahyati <cahyatisanti08@gmail.com>
2019-10-15 13:05:38 +0700

    commint basket.

--- app/src/main/java/com/mobile/myaplikasi/MainActivity.java
+++ app/src/main/java/com/mobile/myaplikasi/MainActivity.java
@@ -3,12 +3,22 @@ package com.mobile.myaplikasi;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private int skorA=0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void tambahpoint(View view) {
+        skorA = skorA + 3;
+        TextView textView = findViewById(R.id.scA);
+        textView.setText(String.valueOf(skorA));
     }
 }
